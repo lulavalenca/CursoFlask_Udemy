99f281e9f26d62436b311712ab1f5a51c29625a6
lulavalenca <lulavalencafilho@gmail.com>
2025-06-09 21:19:25 -0300

    modified:   oo_projeto/modelos/biblioteca.py

--- oo_projeto/modelos/biblioteca.py
+++ oo_projeto/modelos/biblioteca.py
@@ -1,16 +1,31 @@
 class Biblioteca:
-    nome = ""
-    ativo = False
+    bibliotecas = []
+    def __init__(self, nome):
+        self.nome = nome
+        self._ativo = False
+        Biblioteca.bibliotecas.append(self)
     
-biblioteca_cidade = Biblioteca()
-biblioteca_cidade.nome = "Biblioteca da Cidade"
-biblioteca_cidade.ativo = True
+    def __str__(self):
+        return self.nome
+    
+    def listar_bibliotecas():
+        for biblioteca in Biblioteca.bibliotecas:
+            print(f"{biblioteca.nome} | {biblioteca.ativo}")
+            
+    def alterna_estado(self):
+        self._ativo = not self._ativo
+        
+    @property
+    def ativo(self):
+       return "ativada" if self._ativo else "desativada"         
+    
+biblioteca_cidade = Biblioteca("Biblioteca da Cidade")
+biblioteca_cidade.alterna_estado()
+biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
+biblioteca_shopping.alterna_estado()
 
-biblioteca_shopping = Biblioteca()
 
-bibliotecas = [biblioteca_cidade, biblioteca_shopping]
-print(vars(biblioteca_cidade))
-print(vars(biblioteca_shopping))
+#print(biblioteca_cidade)
+#print(biblioteca_shopping)
 
-for biblioteca in bibliotecas:
-    print(vars(biblioteca))
+Biblioteca.listar_bibliotecas()
